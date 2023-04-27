ab0208b0a6764bfe99b7cd9dd1cba485f1ed86a3
Constantin <bunuconstantinjun@gmail.com>
2023-04-27 21:15:49 +0300

    DOM and Array projects

--- course/functions/app.js
+++ course/functions/app.js
@@ -14,10 +14,10 @@ let gameIsRuning = false;
 const getPlayerChoise = () => {
   const selection = prompt(`${ROCK}, ${SCISSOR}, ${PAPER}`, "").toUpperCase();
 
-  if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
-    alert(`Invalide choice! We chose ${DEFAULT_SELECTION} for you`);
-    return DEFAULT_SELECTION;
-  }
+  //   if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
+  //     alert(`Invalide choice! We chose ${DEFAULT_SELECTION} for you`);
+  //     return DEFAULT_SELECTION;
+  //   }
 
   return selection;
 };
@@ -33,14 +33,16 @@ const getComputerChoise = () => {
   }
 };
 
-const getWinner = (cChoice, pChoise) =>
-  cChoice === pChoise
+const getWinner = (cChoice, pChoise = DEFAULT_SELECTION) => {
+  console.log(pChoise);
+  return cChoice === pChoise
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoise === PAPER) ||
       (cChoice === PAPER && pChoise === SCISSOR) ||
       (cChoice === SCISSOR && pChoise === ROCK)
     ? RESULT_PLAYER_WIN
     : RESULT_COPMUTER_WIN;
+};
 
 // if (cChoice === pChoise) {
 //     return RESULT_DRAW;
@@ -86,9 +88,18 @@ startGameBtn.addEventListener("click", () => {
 
   const playerChoise = getPlayerChoise();
   const computerChoise = getComputerChoise();
-  const winner = getWinner(computerChoise, playerChoise);
 
-  let message = `You picked ${playerChoise}, computer picked ${computerChoise}, you`;
+  let winner;
+
+  if (playerChoise) {
+    winner = getWinner(computerChoise, playerChoise);
+  } else {
+    winner = getWinner(computerChoise);
+  }
+
+  let message = `You picked ${
+    playerChoise || DEFAULT_SELECTION
+  }, computer picked ${computerChoise}, you`;
 
   if (winner === RESULT_DRAW) {
     message = message + " had a draw";
@@ -120,3 +131,88 @@ startGameBtn.addEventListener("click", () => {
 // const add1 = (value,value2) => console.log(value, value2);
 
 // add1(2, 3);
+
+// let nume = 'Costea';
+// // function greate(greateMessage) {
+// //     const result = greateMessage + nume;
+// //     return result;
+// // }
+// const greate = function (greateMessage) {
+//     const nume1 = 'Andrii';
+//     const result = greateMessage;
+//     return result;
+// }
+
+// const firstGreate = greate('Salut ');
+// const secondGreate = greate('Hi ');
+
+
+const combine = (operation, ...numbers) => {
+  const showResult = (messageText) => {
+    alert(messageText + sum);
+  };
+
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+
+  for (const num of numbers) {
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+  }
+
+  return showResult;
+};
+
+const resul1 = combine("ADD", 1, 2);
+const result2 = combine("SUBTRACT", 2, 1);
+console.dir(resul1);
+console.dir(result2);
+// resul1('rezultatul la adunare: ');
+// result2('rezultatul la scadere: ');
+
+
+// function addNumber(num1) {
+//   return num1 + Math.random();
+// }
+
+// console.log(addNumber(11));
+// console.log(addNumber(11));
+// console.log(addNumber(11));
+// console.log(addNumber(11));
+
+// let previousResult = 0;
+
+
+// function addMoreNumber(num1,num2) {
+//   const sum = num1 + num2;
+//   previousResult = sum;
+//   return sum
+// }
+
+// console.log(previousResult + ' Before function');
+// console.log(addMoreNumber(2,3));
+// console.log(previousResult + 'After function');
+
+// const createObject = (num,age,hoby) => {
+//   return {
+//     nume:num,
+//     virsta: age,
+//     interese: hoby
+//   }
+// };
+
+// console.log(createObject('Costea', 23, ['sport']));
+
+
+let a = 10;
+function b() {
+  a = 20;
+}
+b();
+console.log(a);
